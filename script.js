8cfb676977feda2a99b7cc477e28f9e98ba29cf6
davidwp009 <142822982+davidwp009@users.noreply.github.com>
2024-07-07 15:33:09 +0300

    ddd

---
+++ script.js
@@ -0,0 +1,25 @@
+//const userNam =  David;
+//console.log(userName.length);
+
+
+
+const userName = 'David';
+const hobby = 'water polo';
+console.log('my name ' + userName + ' i like ' + hobby);
+console.log(`my name ${userName} i like ${hobby}`)
+
+const world = 'world';
+console.log(world.length);
+console.log(world.padEnd(9, '*'));
+console.log(world.toUpperCase());
+
+const a = 5;
+const f = 4;
+const o = 8;
+const p = 1;
+const k = 6;
+const resolt = a + f + o + p + k;
+console.log(`дякуємо за покупки до сплати ${resolt} грн`)
+
+
+
